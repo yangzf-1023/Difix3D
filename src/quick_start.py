4b8faf2edbf764e614f52c004454ce3f18faa8cc
yangzf-1023 <1258174135@qq.com>
2025-07-21 15:04:46 +0800

    示例调整，用于确认进度条的显示问题

--- src/quick_start.py
+++ src/quick_start.py
@@ -4,8 +4,8 @@ from diffusers.utils import load_image
 pipe = DifixPipeline.from_pretrained("/home/yangzhifan/Difix3D/checkpoint", local_files_only=True)
 pipe.to("cuda")
 
-input_image = load_image("/home/yangzhifan/4d-gaussian-splatting/output/N3V/cut_roasted_beef_vggt_woba_8e6/traj/ours_30000/renders/00010.png")
-ref_image = load_image("/home/yangzhifan/4d-gaussian-splatting/data/N3V/cut_roasted_beef/images/cam00_0006.png")
+input_image = load_image("/home/yangzhifan/4d-gaussian-splatting/data/N3V/flame_steak_vggt_ba_5/novel_images/iter100_00_0000.png")
+ref_image = load_image("/home/yangzhifan/4d-gaussian-splatting/data/N3V/flame_steak_vggt_ba_5/novel_images/iter100_00_0000.png")
 prompt = "remove degradation"
 
 output_image = pipe(prompt, image=input_image, ref_image=ref_image, num_inference_steps=1, timesteps=[199], guidance_scale=0.0).images[0]
